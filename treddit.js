d187527d956a893a1dc64322413d8a1c31212f7b
Troy Dorman <troydorman@gmail.com>
2020-11-29 22:17:04 -0600

    basic reading and storing of database from txt file

--- treddit.js
+++ treddit.js
@@ -1,2 +1,31 @@
-const treddit = require('snoowrap');
+const snoowrap = require('snoowrap');
 
+const fs = require('fs');
+
+fs.readFile('army-roster.txt', 'utf8', function (err, data) {
+    let db = data.split(",");
+    console.log(db);
+
+});
+
+
+
+
+
+
+/* const r = new snoowrap({
+    userAgent: 'treddit',
+    clientId: 'VCKLiN-rhsI26A',
+    clientSecret: 'ECes_AB7exURjiC1a0nR_plxgS4',
+    username: 'RadiantStandard4',
+    password: 'blahblah'
+  });
+
+  // Submitting a link to a subreddit
+r.getSubreddit('gifs').submitLink({
+    title: 'Mt. Cameramanjaro',
+    url: 'https://i.imgur.com/n5iOc72.gifv'
+  });
+
+  // Printing a list of the titles on the front page
+  r.getHot().map(post => post.title).then(console.log); */
