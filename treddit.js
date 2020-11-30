adde2cf5860cddc858829ad5764d28fe7445580b
Troy Dorman <troydorman@gmail.com>
2020-11-29 23:32:26 -0600

    converted soldier database to json data

--- treddit.js
+++ treddit.js
@@ -1,5 +1,8 @@
 const snoowrap = require('snoowrap');
 const fs = require('fs');
+const platoon = [];
+
+
 
 
 class Soldier {
@@ -12,19 +15,35 @@ class Soldier {
     }
 }
 
-function generateRandomAgent(){
+function generateRandomAgent() {
 
     let randoNum = Math.floor((Math.random() * 1000) + 1);
-    return(`user-agent-${randoNum}`);
+    return (`user-agent-${randoNum}`);
 
 }
 
-fs.readFile('army-roster.txt', 'utf8', function (err, data) {
+fs.readFile('army-roster.json', 'utf8', function (err, data) {
 
     if (err === null) {
+       let db = JSON.parse(data);
 
-        let db = data.split(",");
-        let fighter = new Soldier(db[0], db[1], db[2], db[3]);
+       console.log(db);
+
+
+    }
+
+
+
+
+});
+
+function draftSoldiers(db) {
+
+
+    db.forEach(function (index) {
+
+        let pieces = index.split(",");
+        let fighter = new Soldier(pieces[0], pieces[1], pieces[2], pieces[3]);
 
         console.log(`
         userid: ${fighter.userId}
@@ -33,10 +52,17 @@ fs.readFile('army-roster.txt', 'utf8', function (err, data) {
         password: ${fighter.password}
         user-agent: ${fighter.useragent}`);
 
-    }
+        platoon.push(fighter);
+    });
 
+    fs.writeFile("test.json", JSON.stringify(platoon, null, 2), function (err) {
 
-});
+        if (err) throw err;
+        console.log('written');
+
+    })
+
+}
 
 
 
@@ -44,18 +70,18 @@ fs.readFile('army-roster.txt', 'utf8', function (err, data) {
 
 
 /* const r = new snoowrap({
-    userAgent: 'treddit',
-    clientId: 'VCKLiN-rhsI26A',
-    clientSecret: 'ECes_AB7exURjiC1a0nR_plxgS4',
-    username: 'RadiantStandard4',
-    password: 'blahblah'
-  });
+    userAgent: fighter.useragent,
+    clientId: fighter.userId,
+    clientSecret: fighter.secretId,
+    username: fighter.username,
+    password: fighter.password
+});
 
-  // Submitting a link to a subreddit
+// Submitting a link to a subreddit
 r.getSubreddit('gifs').submitLink({
     title: 'Mt. Cameramanjaro',
     url: 'https://i.imgur.com/n5iOc72.gifv'
-  });
+});
 
-  // Printing a list of the titles on the front page
-  r.getHot().map(post => post.title).then(console.log); */
+// Printing a list of the titles on the front page
+r.getHot().map(post => post.title).then(console.log); */
